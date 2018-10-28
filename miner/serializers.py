d16d2699f44f59936a7c435aa3408c8e8ad9da92
binhan <yahabin@gmail.com>
2018-10-28 23:14:27 +0800

    5th

--- miner/serializers.py
+++ miner/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
-from .models import Miner
+# from rest_framework import serializers
+# from .models import Miner
 
-class Minerserializer(serializers.ModelSerializer):
+# class Minerserializer(serializers.ModelSerializer):
 
-	class Meta:
-		model = Miner
-		fields = '__all__'
+# 	class Meta:
+# 		model = Miner
+# 		fields = '__all__'
